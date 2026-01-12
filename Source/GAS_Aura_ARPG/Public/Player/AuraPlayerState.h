5bf7e253dc7aa0371f3135d302a76290b0ddb7d5
Astiom13 <andric199688@gmail.com>
2026-01-12 16:03:41 +0200

    Add Player State

---
+++ Source/GAS_Aura_ARPG/Public/Player/AuraPlayerState.h
@@ -0,0 +1,19 @@
+// Copyright Astiom_13
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AuraPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_AURA_ARPG_API AAuraPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+	AAuraPlayerState();
+};
