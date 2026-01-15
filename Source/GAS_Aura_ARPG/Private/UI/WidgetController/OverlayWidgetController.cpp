dc75a880480fe861f2d2780ef826a421eb7bccc3
Astiom13 <andric199688@gmail.com>
2026-01-15 18:00:03 +0200

    Aura widget controller class

---
+++ Source/GAS_Aura_ARPG/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -0,0 +1,5 @@
+// Copyright Astiom_13
+
+
+#include "UI/WidgetController/OverlayWidgetController.h"
+
---
+++ Source/GAS_Aura_ARPG/Public/UI/WidgetController/OverlayWidgetController.h
@@ -0,0 +1,17 @@
+// Copyright Astiom_13
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/WidgetController/AuraWidgetController.h"
+#include "OverlayWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_AURA_ARPG_API UOverlayWidgetController : public UAuraWidgetController
+{
+	GENERATED_BODY()
+	
+};
